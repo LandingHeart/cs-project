975f5b0de62d364dc0ec63bd7c63995d70554636
LandingHeart <land.liu0123@gmail.com>
2019-11-24 13:08:04 -0500

    add permisson and states for signin

--- frontend/src/components/SignIn.js
+++ frontend/src/components/SignIn.js
@@ -5,6 +5,15 @@ import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 export default class SignIn extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      permission: false,
+      customer: []
+    }
+
+  }
+  componentDidMount(){
+    
   }
 
   render() {
