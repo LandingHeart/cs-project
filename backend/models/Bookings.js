3ffd77050f1538f32b162d0f21a9785fd5106716
LandingHeart <land.liu0123@gmail.com>
2020-01-04 11:21:35 -0500

    last change to project

--- backend/models/Bookings.js
+++ backend/models/Bookings.js
@@ -1,10 +1,22 @@
 const mongoose = require("mongoose");
 
 const BookingSchema = mongoose.Schema({
-    customer: String,
-    flightid: Number,
-    id: Number,
-    bookedFrom: String
-})
+  customer: {
+    tyep: String,
+    require: true
+  },
+  flightid: {
+    type: Number,
+    require: true
+  },
+  id: {
+    type: Number,
+    require: true
+  },
+  bookedFrom: {
+    type: String,
+    require: true
+  }
+});
 
 module.exports = mongoose.model("bookings", BookingSchema);
